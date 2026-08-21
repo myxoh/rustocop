28e986e8557e5ee81b08ac0fae38f82429450365
Nico <nico@singulate.com>
2026-08-21 12:09:10 -0400

    Match File read call shapes

--- spec/project_parity_regressions_spec.rb
+++ spec/project_parity_regressions_spec.rb
@@ -84,6 +84,7 @@ RSpec.describe "real-project parity regressions" do
     Style/ClassMethodsDefinitions
     Lint/SelfAssignment
     Style/BitwisePredicate
+    Style/FileRead
   ]
   fixture_root = File.join(ROOT, "spec", "fixtures", "project_parity_regressions")
   config = File.join(fixture_root, "rubocop.yml")
